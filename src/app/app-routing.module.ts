536d231732819b30454cb09ea2f865373704036d
futoruma <evgeny.martov@gmail.com>
2023-01-18 17:26:18 +0900

    Added routing.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './components/about/about.component';
+import { MainpageComponent } from './components/mainpage/mainpage.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: MainpageComponent},
+  { path: 'about', component: AboutComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
